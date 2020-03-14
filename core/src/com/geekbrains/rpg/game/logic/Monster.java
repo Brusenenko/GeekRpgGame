34a459de77ee8977dfae55eeb91f98373230e6a7
Andrew <brusenenko@yandex.ru>
2020-03-14 15:37:00 +0300

    Lesson4_HW

--- core/src/com/geekbrains/rpg/game/logic/Monster.java
+++ core/src/com/geekbrains/rpg/game/logic/Monster.java
@@ -3,15 +3,27 @@ package com.geekbrains.rpg.game.logic;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
+import com.geekbrains.rpg.game.logic.utils.Poolable;
 import com.geekbrains.rpg.game.screens.utils.Assets;
 
-public class Monster extends GameCharacter {
+public class Monster extends GameCharacter implements Poolable {
     private float attackTime;
 
     public Monster(GameController gc) {
         super(gc, 20, 100.0f);
         this.texture = Assets.getInstance().getAtlas().findRegion("knight");
         this.changePosition(800.0f, 300.0f);
+        this.visionRadius = 100.0f;
+        this.dst.set(this.position);
+    }
+
+    public void generateMe() {
+        do {
+            changePosition(MathUtils.random(0, 1280), MathUtils.random(0, 720));
+        } while (gc.getMap().isGroundPassable(position));
+        hpMax = 20;
+        hp = hpMax;
+
     }
 
     @Override
@@ -30,7 +42,12 @@ public class Monster extends GameCharacter {
 
     public void update(float dt) {
         super.update(dt);
-        dst.set(gc.getHero().getPosition());
+        if (this.position.dst(gc.getHero().getPosition()) < visionRadius) {
+            dst.set(gc.getHero().getPosition());
+        }
+        if (position.dst(dst) < 1.0f) {
+             dst.set(MathUtils.random(0, 1280), MathUtils.random(0, 720));
+        }
         if (this.position.dst(gc.getHero().getPosition()) < 40) {
             attackTime += dt;
             if(attackTime > 0.3f) {
@@ -38,5 +55,10 @@ public class Monster extends GameCharacter {
                 gc.getHero().takeDamage(1);
             }
         }
+    }
+
+    @Override
+    public boolean isActive() {
+        return hp > 0;
     }
 }
