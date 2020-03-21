fa8e8e8ab79344f80c8124a1405f901b38a388cc
Andrew <brusenenko@yandex.ru>
2020-03-21 15:41:26 +0300

    Lesson6_Master

--- core/src/com/geekbrains/rpg/game/logic/GameCharacter.java
+++ core/src/com/geekbrains/rpg/game/logic/GameCharacter.java
@@ -12,9 +12,12 @@ public abstract class GameCharacter implements MapElement {
         IDLE, MOVE, ATTACK, PURSUIT, RETREAT
     }
 
+    static final int WIDTH = 60;
+    static final int HEIGHT = 60;
+
     protected GameController gc;
 
-    protected TextureRegion texture;
+    protected TextureRegion[][] textures;
     protected TextureRegion textureHp;
 
     protected State state;
@@ -31,8 +34,11 @@ public abstract class GameCharacter implements MapElement {
     protected Circle area;
 
     protected float lifetime;
-    protected float visionRadius;
     protected float attackTime;
+    protected float walkTime;
+    protected float timePerFrame;
+
+    protected float visionRadius;
     protected float speed;
     protected int hp, hpMax;
 
@@ -46,9 +52,25 @@ public abstract class GameCharacter implements MapElement {
         return (int) (position.y - 20) / 80;
     }
 
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
     public void changePosition(float x, float y) {
         position.set(x, y);
-        area.setPosition(x, y - 20);
+        if (position.x < 0.1f) {
+            position.x = 0.1f;
+        }
+        if (position.y - 20 < 0.1f) {
+            position.y = 20.1f;
+        }
+        if (position.x > Map.MAP_CELLS_WIDTH * 80 - 1) {
+            position.x = Map.MAP_CELLS_WIDTH * 80 - 1;
+        }
+        if (position.y - 20 > Map.MAP_CELLS_HEIGHT * 80 - 1) {
+            position.y = Map.MAP_CELLS_HEIGHT * 80 - 1 + 20;
+        }
+        area.setPosition(position.x, position.y - 20);
     }
 
     public void changePosition(Vector2 newPosition) {
@@ -80,7 +102,12 @@ public abstract class GameCharacter implements MapElement {
         this.speed = speed;
         this.state = State.IDLE;
         this.stateTimer = 1.0f;
+        this.timePerFrame = 0.2f;
         this.target = null;
+    }
+
+    public int getCurrentFrameIndex() {
+        return (int)(walkTime / timePerFrame) % textures[0].length;
     }
 
     public void update(float dt) {
@@ -96,33 +123,31 @@ public abstract class GameCharacter implements MapElement {
             if (attackTime > weapon.getSpeed()) {
                 attackTime = 0.0f;
                 if (weapon.getType() == Weapon.Type.MELEE) {
-                    target.takeDamage(this, 1);
+                    target.takeDamage(this, weapon.generateDamage());
                 }
-                if (weapon.getType() == Weapon.Type.RANGED) {
-                    gc.getProjectilesController().setup(this, position.x, position.y, target.getPosition().x, target.getPosition().y);
+                if (weapon.getType() == Weapon.Type.RANGED && target != null) {
+                    gc.getProjectilesController().setup(this, position.x, position.y, target.getPosition().x, target.getPosition().y, weapon.generateDamage());
                 }
             }
         }
-        area.setPosition(position.x, position.y - 20);
     }
 
     public void moveToDst(float dt) {
         tmp.set(dst).sub(position).nor().scl(speed);
         tmp2.set(position);
+        walkTime += dt;
         if (position.dst(dst) > speed * dt) {
-            position.mulAdd(tmp, dt);
+            changePosition(position.x + tmp.x * dt, position.y + tmp.y * dt);
         } else {
-            position.set(dst);
+            changePosition(dst);
             state = State.IDLE;
         }
         if (!gc.getMap().isGroundPassable(getCellX(), getCellY())) {
-            position.set(tmp2);
-            position.add(tmp.x * dt, 0);
+            changePosition(tmp2.x + tmp.x * dt, tmp2.y);
             if (!gc.getMap().isGroundPassable(getCellX(), getCellY())) {
-                position.set(tmp2);
-                position.add(0, tmp.y * dt);
+                changePosition(tmp2.x, tmp2.y + tmp.y * dt);
                 if (!gc.getMap().isGroundPassable(getCellX(), getCellY())) {
-                    position.set(tmp2);
+                    changePosition(tmp2);
                 }
             }
         }
